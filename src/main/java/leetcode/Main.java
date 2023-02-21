a5816004c5394c286a210409c7fbe510d8ff0bc5
yuri hagel <y.khagel@tinkoff.ru>
2023-02-21 14:58:01 +0300

    day1

--- src/main/java/leetcode/Main.java
+++ src/main/java/leetcode/Main.java
@@ -1,7 +1,13 @@
 package leetcode;
 
+import leetcode.easy.TwoSum;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        int[] myArr = {2,7,11,15};
 
+        System.out.println(Arrays.toString(TwoSum.twoSum2(myArr, 9)));
     }
 }
